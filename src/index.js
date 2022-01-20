1c7a644df61eb9b4d1ab0ebb88dc5b39191c1ad4
Bailym <bm39@hotmail.co.uk>
2022-01-20 21:17:47 +0000

    added some comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import NavBar from "./components/navbar";
-import Card from "./card";
+import Card from "./card";  //components from other files.
 import Draw from "./draw";
 
 
@@ -13,9 +13,35 @@ const { Header, Content, Footer } = Layout;
 
 class Index extends React.Component {
 
+
+  /* 
+  render the basic layout of the app
+  HEADER: Where you render the nav bar (inside the components folder /src/components/navbar.js)
+
+  CONTENT:  The main content of the page. Render a component depending on the URL (/card or /draw)
+
+  FOOTER: Anything you want really. I just put my name.
+
+  Notes: 
+    - every React component needs a render() method. HTML + CSS goes here. This is what is shown when the component is loaded.
+    - CSS can be added within this file by adding a style attribute inside a HTML tag e.g. <div style = {{color: "red"}}>. Or just use a seperate CSS file and import it at the top.
+    - If you're using VS Code, install Github copilot extenstion. Its awesome!
+  */
+
+
+  //you can write js functions in here too like normal.
+  ExampleFunction = () => {
+    //generate a random number between 1 and 10
+    var randomNumber = Math.floor(Math.random() * 10) + 1;
+    //return the random number
+    return randomNumber;
+  }
+
+
+  //render is a function that is called when the component is rendered
   render = () => {
-    
-    return(<Router>
+
+    return (<Router>
       <Layout className="layout" style={{ minHeight: "100vh" }}>
 
         <Header style={{ minHeight: "5vh" }}>
@@ -39,9 +65,9 @@ class Index extends React.Component {
   }
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'));
-export default Index;
-  
+ReactDOM.render(<Index />, document.getElementById('root'));  //render the componen - calls the render() function.
+export default Index; //Allows this component to be used in other files. Probably not needed.
+
 
 
 
