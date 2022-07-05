fda1d6630c87422617b717e5552dc0dadf9b8ed2
Bailym <bm39@hotmail.co.uk>
2022-07-05 14:00:20 +0100

    add instructions and tidy up home page

--- src/draw.js
+++ src/draw.js
@@ -7,7 +7,7 @@ import BingoCard from './components/bingoCard';
 import { Statistic, Card, Row, Col } from 'antd';
 import { Typography } from 'antd';
 
-const { Title } = Typography;
+const { Title, Paragraph, Text } = Typography;
 
 
 
@@ -16,7 +16,7 @@ class Draw extends React.Component {
 
     state = {
         numbersCalled: [],
-        lastNumber: "",
+        lastNumber: "????",
     }
 
     pad = (str, max) => {
@@ -32,10 +32,10 @@ class Draw extends React.Component {
         let paddedNum = this.pad(binaryNumber, 4)
 
 
- 
+
 
         //check if paddedNum is in the array
-        if(this.state.numbersCalled.length >= 16){
+        if (this.state.numbersCalled.length >= 16) {
             alert("You have called all the numbers!")
         }
         else if (this.state.numbersCalled.includes(paddedNum)) {
@@ -52,8 +52,18 @@ class Draw extends React.Component {
 
     render = () => {
         return (
-            <div style={{ width: "30vw", margin: "2vh auto" }}>
-                <Title style={{ textAlign: "center", fontSize: "296px" }}>{this.state.lastNumber}</Title>
+            <div style={{ width: "50vw", margin: "2vh auto",textAlign:"center", justifyItems:"center"}}>
+                <Title>How it Works...</Title>
+                <Paragraph>
+                    <ul style={{listStyleType:"none", fontSize:"32px"}}>
+                        <li>1. Students should go to the <a href="/card">card</a> page. This will give them a Bingo Card which they can tick off.</li>
+                        <li>2. The teacher clicks the button below and calls out the random 4-bit binary number</li>
+                        <li>3. Students will need to convert the 4-bit binary number into denary</li>
+                        <li>4. If the number appears on a students card they should click the number to tick it off.</li>
+                        <li>5. Repeat this process until a student has all numbers on their card ticked off.</li>
+                    </ul>
+                </Paragraph>
+                <Text style={{fontSize: "296px"}}>{this.state.lastNumber}</Text>
                 <Button type="primary" style={{ margin: "0 auto", width: "100%", height: "5vh" }} onClick={() => this.getNumber()}>
                     Draw
                 </Button>
