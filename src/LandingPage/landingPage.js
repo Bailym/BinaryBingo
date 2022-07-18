6f71cc43d9817a6277fb2f25c545e6a8c8a5488b
Bailym <bm39@hotmail.co.uk>
2022-07-18 13:08:48 +0100

    re style landing page

--- src/LandingPage/landingPage.js
+++ src/LandingPage/landingPage.js
@@ -1,6 +1,16 @@
+import "./landingPage.css";
+import {Link } from 'react-router-dom';
+
 function LandingPage() {
   return (
-    <div id="landing-div">
+    <div className="landing-div">
+        <h1>BINARY BINGO.</h1>
+        <button className="landing-button landing-button-red">
+            <Link to="/Draw">Teacher</Link>
+        </button>
+        <button className="landing-button landing-button-blue">
+            <Link to="/Card">Student</Link>
+        </button>
       
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import "./index.css";
 import NavBar from "./NavBar/navBar";
 import Draw from "./Draw/draw";
 import { Layout } from 'antd';
@@ -13,23 +14,23 @@ const { Header, Content, Footer } = Layout;
 
 function Index() {
   return (
-  <Router>
-    <Layout className="layout" style={{ minHeight: "100vh" }}>
-      <Header style={{ minHeight: "5vh" }}>
-        <NavBar />
-      </Header>
-      <Content style={{minHeight: "85vh" }}>
-        <Routes>
-          <Route path="/card" element={<BingoCard />} />
-          <Route path="/draw" element={<Draw />} />
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </Content>
-      <Footer style={{ textAlign: 'center', minHeight: "5vh" }}>
-        B Martin 2021
-      </Footer>
-    </Layout>
-  </Router>)
+    <Router>
+      <Layout className="layout">
+        {/* <Header style={{ minHeight: "5vh" }}>
+          <NavBar />
+        </Header> */}
+        <Content style={{ minHeight: "85vh" }}>
+          <Routes>
+            <Route path="/card" element={<BingoCard />} />
+            <Route path="/draw" element={<Draw />} />
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </Content>
+        <Footer className="footer">
+          B Martin 2021
+        </Footer>
+      </Layout>
+    </Router>)
 
 }
 ReactDOM.render(<Index />, document.getElementById('root'));  //render the componen - calls the render() function.
